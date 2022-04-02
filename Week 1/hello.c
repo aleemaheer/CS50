ca7af808aaef4e95d87edc8385c665a2bd2e7b8d
aleem aheer <aleemaheer5769644@gmail.com>
2022-04-03 00:53:43 +0500

    Week 1 done and Week 2 started, Week 2 done upto 33 minutes

--- Week 1/hello.c
+++ Week 1/hello.c
@@ -3,5 +3,3 @@
 int main(void) {
     printf("Hello, World\n");
 }
-
-// 20 min done week 1
---
+++ Week 2/buggy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    for (i = 0; i <= 3; i++) {
+        printf("#\n");
+    }
+}
+
+// Week 2 33 min done
